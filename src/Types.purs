22ca61b896742e69eebd8e38e8aefcaad665151a
Evan Wildenhain <evan.wildenhain@gmail.com>
2021-01-30 22:15:21 -0800

    represent each tile as a subgraph for each terrain type?

--- src/Types.purs
+++ src/Types.purs
@@ -1,4 +1,3 @@
-
 module Types where
 
 import Data.Variant
@@ -8,45 +7,83 @@ import Data.Symbol (class IsSymbol)
 import Type.Row (class Union, type (+))
 import Data.Newtype
 
+data OneOfThree
+  = Lo
+  | Mid
+  | Hi
+
+derive instance oneOfThreeEq :: Eq OneOfThree
+
+derive instance oneOfThreeOrd :: Ord OneOfThree
+
+data Dir
+  = N
+  | E
+  | S
+  | W
+
+derive instance dirEq :: Eq Dir
+
+derive instance dirOrd :: Ord Dir
+
 _road = SProxy :: SProxy "road"
+
 _city = SProxy :: SProxy "city"
+
 _cityCrest = SProxy :: SProxy "cityCrest"
+
 _field = SProxy :: SProxy "field"
+
 _monastery = SProxy :: SProxy "monastery"
 
 inju :: forall a b c. Cons c Unit b a => IsSymbol c => SProxy c -> Variant a
 inju x = inj x unit
 
-type CommonTerrainTypes a r = (city :: a, field :: a | r)
+type CommonTerrainTypes a r
+  = ( city :: a, field :: a | r )
 
-type SideTerrainTypes a r = (road :: a | r)
-type MiddleTerrainTypes a r = (monastery :: a, cityCrest :: a | r)
-type AllTerrainTypes a r = (MiddleTerrainTypes a + SideTerrainTypes a + CommonTerrainTypes a + r)
+type SideTerrainTypes a r
+  = ( road :: a | r )
 
-type SideTerrain = Variant (SideTerrainTypes Unit + CommonTerrainTypes Unit + ())
-type MiddleTerrain = Variant (MiddleTerrainTypes Unit + CommonTerrainTypes Unit + ())
-type AnyTerrain = Variant (AllTerrainTypes Unit ())
+type MiddleTerrainTypes a r
+  = ( monastery :: a, cityCrest :: a | r )
+
+type AllTerrainTypes a r
+  = (MiddleTerrainTypes a + SideTerrainTypes a + CommonTerrainTypes a + r)
+
+type SideTerrain
+  = Variant (SideTerrainTypes Unit + CommonTerrainTypes Unit + ())
+
+type MiddleTerrain
+  = Variant (MiddleTerrainTypes Unit + CommonTerrainTypes Unit + ())
+
+type AnyTerrain
+  = Variant (AllTerrainTypes Unit ())
 
 showTerrain :: AnyTerrain -> String
-showTerrain = match
-  { road: const "road", 
-    city: const "city", 
-    cityCrest: const "city crest",
-    field: const "field", 
-    monastery: const "monastery"}
+showTerrain =
+  match
+    { road: const "road"
+    , city: const "city"
+    , cityCrest: const "city crest"
+    , field: const "field"
+    , monastery: const "monastery"
+    }
 
+type TileImageSpec
+  = { url :: String
+    , numCcwRotates :: Int
+    }
 
-type TileImageSpec = {
-  url :: String,
-  numCcwRotates :: Int
-}
+newtype Tile
+  = Tile TileInternal
 
-newtype Tile = Tile TileInternal
-type TileInternal = {
-    sides :: Array SideTerrain,
-    middle :: MiddleTerrain,
-    image :: TileImageSpec
-  }
+type TileInternal
+  = { sides :: Array SideTerrain
+    , middle :: MiddleTerrain
+    , image :: TileImageSpec
+    }
+
 derive instance newtypeTile :: Newtype Tile _
 
 instance showTile :: Show Tile where
